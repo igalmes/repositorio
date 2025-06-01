52886ea838d731732c21f21caf9b71c8f6c5066b
ignaciogalmes <ignaciogalmes79@gmail.com>
2025-06-01 12:38:34 -0300

    new log

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
-const sendMail = require('/sendmail.js');
+const sendMail = require('./sendmail.js');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
